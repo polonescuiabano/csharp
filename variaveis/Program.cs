84f90e0507dc30c0312543a7b80c15394f23c347
polonescuiabano <fernandopkb@hotmail.com>
2024-11-01 16:06:19 -0400

    Tipos de dados primitivos
    estudo sobre os dados primitivos

--- variaveis/Program.cs
+++ variaveis/Program.cs
@@ -1,2 +1,21 @@
-﻿String nome = "Fernando Przybyszewski Barros";
-Console.WriteLine(nome);
+﻿using System.Diagnostics.Contracts;
+
+//Tipos de dados primitivos
+short ano = 2025;
+int contador = 10;
+long populacaoMundial = 7800000000;
+
+Console.WriteLine(ano);
+Console.WriteLine(contador);
+Console.WriteLine(populacaoMundial);
+
+//numeros de ponto flutuante
+float temperatura = 43.6f;
+double precoProduto = 199.90;
+
+//dados do tipo caracter
+char inicial = 'F';
+
+//tipos de dados logicos
+bool isLogged = true;
+bool active = false;

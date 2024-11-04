5a93636de7d186ca3e923eb8523b1f89fc8b1b80
polonescuiabano <fernandopkb@hotmail.com>
2024-11-04 16:59:51 -0400

    Interpolacao de strings
    pequeno estudo sobre interpolacao de strings

--- concatenacao/Program.cs
+++ concatenacao/Program.cs
@@ -1,3 +1,9 @@
 ﻿string nome = "Fernando";
 string saudacao = "Ola, "+nome+"! Seja bem vindo.";
 Console.WriteLine(saudacao);
+
+//Interpolacao de strings
+
+string nome1= "Fernando";
+string saudacao1= $"Ola, {nome1}! Seja bem vindo.";
+Console.WriteLine(saudacao1);

60d084e4e78bd77e65edff133b6a48cc4565dd36
polonescuiabano <fernandopkb@hotmail.com>
2024-11-04 17:20:21 -0400

    Operador GetType, palavra-chave typeof e metodo Parse
    estudo basico sobre operador getType, palavra-chave typeof e metodo parse

--- concatenacao/Program.cs
+++ concatenacao/Program.cs
@@ -3,7 +3,21 @@ string saudacao = "Ola, "+nome+"! Seja bem vindo.";
 Console.WriteLine(saudacao);
 
 //Interpolacao de strings
-
 string nome1= "Fernando";
 string saudacao1= $"Ola, {nome1}! Seja bem vindo.";
 Console.WriteLine(saudacao1);
+
+//Operador GetType
+int numero = 5;
+Console.WriteLine(numero.GetType());
+
+//Palavra-chave typeof
+Console.WriteLine(typeof(int));
+
+//Metodo Parse
+string valorTexto = "100";
+Console.WriteLine("A variavel valorTexto e do tipo:"+valorTexto.GetType());
+
+int numero1 = int.Parse(valorTexto);
+Console.WriteLine(numero1);
+Console.WriteLine("A variavel numeroParse e do tipo:"+numero1.GetType());

095ff5825bbafe0b5cf5ffc447b3bc8d425d8915
polonescuiabano <fernandopkb@hotmail.com>
2024-11-06 15:59:35 -0400

    metodo convert e metodo tostring
    estudo basico de casting

--- concatenacao/Program.cs
+++ concatenacao/Program.cs
@@ -21,3 +21,35 @@ Console.WriteLine("A variavel valorTexto e do tipo:"+valorTexto.GetType());
 int numero1 = int.Parse(valorTexto);
 Console.WriteLine(numero1);
 Console.WriteLine("A variavel numeroParse e do tipo:"+numero1.GetType());
+
+//Metodo Convert
+
+string valorTexto1 = "100";
+Console.WriteLine("A variavel valorTexto1 e do tipo" + valorTexto1.GetType());
+
+int numeroConvertido = Convert.ToInt32(valorTexto1);
+Console.WriteLine(numeroConvertido);
+Console.WriteLine("A variavel numeroConvertido e do tipo"+numeroConvertido.GetType());
+
+//Metodo ToString
+int numero2 = 123;
+Console.WriteLine(numero2.GetType());
+
+string numeroComoString = numero2.ToString();
+Console.WriteLine(numeroComoString.GetType());
+
+
+DateTime dataAtual = DateTime.Now;
+Console.WriteLine(dataAtual);
+string dataFormatada = dataAtual.ToString("dd/MM/yyyy");
+Console.WriteLine(dataFormatada);
+
+double preco = 29.99;
+Console.WriteLine($"o preco e: {preco.ToString("C2")}");
+
+decimal valor = 1234.56m;
+string valorUS = valor.ToString("C", new.System.Globalization.CultureInfo("en-US"));
+Console.WriteLine(valorUS);
+
+string valorBR = valor.ToString("C", new.System.Globalization.Culture("pt-BR"));
+Console.WriteLine(valorBR);

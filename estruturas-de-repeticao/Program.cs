4c53193d2c491626b5d4c5a27e4586a99eabb286
polonescuiabano <fernandopkb@hotmail.com>
2024-11-11 14:31:53 -0400

    estrutura de repeticao while
    estudo basico sobre estrutura de repeticao while

---
+++ estruturas-de-repeticao/Program.cs
@@ -0,0 +1,11 @@
+﻿Random numeroAleatorio = new Random();
+int numero = numeroAleatorio.Next(1,101);
+int contador = 0;
+Console.WriteLine($"Numero Alvo: {numero}");
+
+while (contador != numero)
+{
+    Console.WriteLine($"Contador: {contador}.");
+    contador++;
+}
+Console.WriteLine($"O contador atingiu o numero alvo: {contador}");

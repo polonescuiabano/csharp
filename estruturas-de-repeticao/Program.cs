cde61b54c8c7eeb0b9cf53e2dbafeda84a97eba0
polonescuiabano <fernandopkb@hotmail.com>
2024-11-11 17:18:02 -0400

    estrutura foreach e exercicios
    estudo basico sobre a estrutura de repeticao foreach e exercicios

--- estruturas-de-repeticao/Program.cs
+++ estruturas-de-repeticao/Program.cs
@@ -22,8 +22,45 @@
 //     Console.WriteLine($"Valor de i: {i}");
 // }
 
-string[] nomes = {"Fernando", "Daniely", "Suzana"};
-foreach (string nome in nomes)
-{
-    Console.WriteLine(nome);
-}
+// string[] nomes = {"Fernando", "Daniely", "Suzana"};
+// foreach (string nome in nomes)
+// {
+//     Console.WriteLine(nome);
+// }
+
+
+// for(int i=1;i<=100;i++)
+// {
+//     if(i%2==0){
+//         int soma = soma+i;
+//     }
+// }
+// Console.WriteLine(soma);
+
+
+// int? i = null;
+// for(i=10; i>=1; i--)
+// {
+//     Console.WriteLine($"Contagem regressiva: {i}");
+// }
+// Console.WriteLine("Decolar!");
+
+// string[] vogais = {"a", "e", "i", "o", "u"};
+// foreach(string vogal in vogais)
+// {
+//     int soma=1+soma;
+// }
+// Console.WriteLine(soma);
+
+// Console.WriteLine("Escreva sua palavra ou frase: ");
+// string palavra = Console.ReadLine();
+
+// foreach(palavras in reverse(palavra))
+// {
+//     if (palavras==palavra){
+//         Console.WriteLine("Sua palavra ou frase e um palindromo");
+//     }
+// }
+
+//crie um algoritmo onde o computador escolhe um numero aleatorio entre 1 e 100, e o usuario deve adivinhar.
+//apos cada tentativa o programa indica se o numero e maior ou menor

20fd81e6e33474c6ef6ed4bc879c93abc0942c02
polonescuiabano <fernandopkb@hotmail.com>
2024-11-22 16:07:28 -0400

    POO-csharp
    getters e setters

--- poo-csharp/Modelo/Veiculo.cs
+++ poo-csharp/Modelo/Veiculo.cs
@@ -1,5 +1,23 @@
 namespace poo_csharp.Modelo{
     public class Veiculo{
-        
+        private string marca {get;set;}
+        private string modelo {get;set;}
+        private int ano {get;set;}
+        protected double velocidadeAtual {get;set;}
+
+        public Veiculo(string marca, string modelo, int ano, double velocidadeAtual){
+            this.marca = marca;
+            this.modelo = modelo;
+            this.ano = ano;
+            this velocidadeAtual = velocidadeAtual;
+        }
+        public void Acelerar(double incremento){
+            velocidadeAtual += incremento;
+            Console.WriteLine($"Acelerando... Velocidade atual {velocidadeAtual} km/h");
+        }
+        public void Freiar(double decremento){
+            velocidadeAtual -= decremento;
+            Console.WriteLine($"Freiando... Velocidade atual {velocidadeAtual} km/h")
+        }
     }
 }
